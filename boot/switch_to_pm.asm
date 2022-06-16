ed8b1671b81069965052cd68372b3e52d35f7a48
NitayKin <nitayk999@gmail.com>
2022-06-17 00:37:44 +0300

    folders and starting idt handling

---
+++ boot/switch_to_pm.asm
@@ -0,0 +1,24 @@
+[bits 16]
+switch_to_pm: ; switch to pm - protected mode
+cli ; switch off interrupts
+lgdt [gdt_descriptor] ; load global descriptor table
+
+mov eax,cr0
+or eax,0x1 ; swtich on first bit of cr0, which indicates protected mode
+mov cr0,eax 
+
+jmp CODE_SEG:init_pm ; far jump to flush pipeline
+
+[bits 32]
+init_pm:
+mov ax,DATA_SEG
+mov ds, ax ; all descriptors now point to data segment descriptor in gdt
+mov ss, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+
+mov ebp, 0x90000
+mov esp, ebp
+
+call BEGIN_PM
